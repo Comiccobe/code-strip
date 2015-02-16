ba90c851830c3971526f43f5b895d36a67c5b277
Jakob Hillerström <zurric@gmail.com>
2015-02-16 23:01:41 +0100

    Fixed unknown states in if-evaluator

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var should = require("chai").should(),
 	codeStrip = require("../lib"),
+	evalIf = require("../lib/if-evaluator"),
 	fs = require("fs")
 
 describe("code strip", function () {
@@ -16,5 +17,19 @@ describe("code strip", function () {
 		var converted = codeStrip(directories, fileTypes, defines, opts)[0].file
 		
 		converted.should.equal(fs.readFileSync(__dirname+"/testcs-intended.cs").toString())
+	})
+
+	it("evaluates if-statements correctly", function () {
+		var statement = [
+				"!",
+				"SMARTLOC_PRO",
+				"||",
+				"UNITY_EDITOR_OSX"
+			],
+			defines = {
+				"SMARTLOC_PRO": true
+			}
+
+		evalIf(statement, defines).should.equal("unknown")
 	})
 })
